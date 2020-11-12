73b425d11d193f3c4638843090d0d1f7f84e7904
quyphong44 <72534641+quyphong44@users.noreply.github.com>
2020-11-11 19:55:03 -0800

    Add files via upload

---
+++ Bài tập 1/char 2.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main (void) {
+ // khai bao bien num kieu int
+ int num;
+ // khoi tao gia tri bien num
+ num = 5;
+ // hien thi bien num ra man hinh
+ printf ("bien num co gia tri: %d",num);
+ // khai bao bien x kieu char
+ char c;
+ // khoi tao gia tri cho bien
+ c = 'a';
+ // hien thi c ra man hinh
+ printf("bien c co gia tri: %c", c);
+ // hien thi dong: bien num = 5 va bien c = 4
+  printf("bien num = %d va bien c = \n" , c);
+ getchar();
+ return 0;
+}
+ 
